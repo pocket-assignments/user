8d0177a881c71624774a2059c0f8baf802db0795
Aditya Baravkar <aditya.baravkar@sjsu.edu>
2022-12-19 15:29:10 -0800

    Added a test API.

---
+++ src/test/java/com/pocket/user/PocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pocket.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
